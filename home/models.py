8535f35d0d9ed2b28b37bae6b37c8ef2c2abff84
andinniv <avgc2411@gmail.com>
2024-12-02 19:22:21 -0300

    commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.Charfield(max_length = 100)
+    ordem = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
